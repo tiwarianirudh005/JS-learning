807f99f28f2cac0b04aee71ab1683fabce4561a7
Anirudh Tiwari <aniirudhtiwari@gmail.com>
2026-06-06 18:06:20 +0000

    1 completed

--- JSbasics(week2)/assignments.js
+++ JSbasics(week2)/assignments.js
@@ -1,3 +1,48 @@
 //ques.1 sum values in object arrays  i/o ={ food: [10, 20, 30], travel: [5, 15], bills: [40, 60] }  o/p =
 
-let input = { food: [10, 20, 30], travel: [5, 15], bills: [40, 60] };
+function findsum(arr){
+    let sum = 0;
+
+    for (let i=0; i<arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+let expenses = {
+    food: [10, 20, 30],
+    travel: [5, 15],
+    bills: [40, 60]
+}
+
+let result = {};
+
+result.food = findsum(expenses.food);
+result.travel = findsum(expenses.travel);
+result.bills = findsum(expenses.bills);
+
+console.log(result);
+
+// now with different data { food: [15, 25, 35], travel: [10, 20], bills: [50, 70] }
+
+// function findsum(arr){
+//     let sum = 0;
+
+//     for (let i=0; i<arr.length; i++) {
+//         sum = sum + arr[i];
+//     }
+//     return sum;
+// }
+// expenses = {
+//     food:[15,25,35],
+//     travel:[10,20],
+//     bills:[50,70]   
+// }
+
+// let result = {};
+
+// result.food = findsum(expenses.food);
+// result.travel = findsum(expenses.travel);
+// result.bills = findsum(expenses.bills);
+
+// console.log(result);        // assignment 1 completes
