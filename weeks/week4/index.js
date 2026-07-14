64a180972f18dc1731ea151a5cbd372e6dc7f36d
Anirudh Tiwari <aniirudhtiwari@gmail.com>
2026-07-14 18:00:12 +0000

    update

--- weeks/week4/index.js
+++ weeks/week4/index.js
@@ -4,7 +4,26 @@ function sum(a,b){
 
 let ans = sum(1,4);
 console.log(ans);
-console.log(sum(1,4));
-
-/******CLASSES********/
+console.log(sum(1,4))
   
+let user = {
+    name: "harkirat",
+    age: 22,
+    cities: ["delhi", "gurgaon","jaipur"]
+}
+
+console.log(user.cities[0]); 
+
+//ques. code the promise class yourself
+
+/******CLASSES********/ 
+// in javascrript classes are away to define blueprint for creating object (which are different from the object defined earlier).
+// example of classes is the flappy bird game
+
+class rectangle(){
+    constructor(width , height , color ) {
+        this.width = width;
+        this.height = height;
+        this.color = 
+    }
+}
---
+++ weeks/week4/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+function sum(a,b){
+    return a+b;
+}
+
+let ans = sum(1,4);
+console.log(ans);
+console.log(sum(1,4))
