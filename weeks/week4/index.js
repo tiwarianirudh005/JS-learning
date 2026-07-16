1d9355bd744e3de4532e48515c21056ad4591329
Anirudh Tiwari <aniirudhtiwari@gmail.com>
2026-07-16 13:46:25 +0000

    Async-Classes

--- weeks/week4/index.js
+++ weeks/week4/index.js
@@ -1,22 +1,64 @@
-function sum(a,b){
-    return a+b;
-}
+// function sum(a,b){
+//     return a+b;
+// }
 
-let ans = sum(1,4);
-console.log(ans);
-console.log(sum(1,4))
+// let ans = sum(1,4);
+// console.log(ans);
+// console.log(sum(1,4))
   
-let user = {
-    name: "harkirat",
-    age: 22,
-    cities: ["delhi", "gurgaon","jaipur"]
-}
+// let user = {
+//     name: "harkirat",
+//     age: 22,
+//     cities: ["delhi", "gurgaon","jaipur"]
+// }
 
-console.log(user.cities[0]); 
+// console.log(user.cities[0]); 
 
 //ques. code the promise class yourself
 
 /******CLASSES********/ 
 // in javascrript classes are away to define blueprint for creating object (which are different from the object defined earlier).
 // example of classes is the flappy bird game
+
+// class Rectangle {
+//     constructor (width, height, color) {
+//         this.width = width;
+//         this.height = height;
+//         this.color = color;
+//     }
+//     area () {
+//         return this.width * this.height;
+//     }
+//     static impo() {
+//         return "i am a rectangle"
+//     }
+// }
+
+// let r1 = new Rectangle (1,2, "red");
+// let r2 = new Rectangle (100,200,"blue");
+// console.log(r1.area());
+// console.log(r2.area());
+// console.log(Rectangle.impo());
+
+/*********flappy bird using classes**********/
+
+// class Rectangle {
+//     constructor (width, height, color) {
+//         this.width = width;
+//         this.height = height;
+//         this.color = color;
+//     }
+//     area () {
+//         return this.width * this.height;
+//     }
+//     perimeter () {
+//         return 2*this.width+this.height;
+//     }
+//     paint () {
+//         console.log("painting with this color" + this.color);
+//     }
+// }
+
+// ASSIGNMENT CREATE A CIRCLE CLASS
+
 
--- weeks/week4/tempCodeRunnerFile.js
+++ weeks/week4/tempCodeRunnerFile.js
@@ -1,7 +1,19 @@
-function sum(a,b){
-    return a+b;
+class Rectangle {
+    constructor (width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    area () {
+        return this.width * this.height;
+    }
+    static impo() {
+        return "i am a rectangle"
+    }
 }
 
-let ans = sum(1,4);
-console.log(ans);
-console.log(sum(1,4))
+let r1 = new Rectangle (1,2, "red");
+let r2 = new Rectangle (100,200,"blue");
+console.log(r1.area());
+console.log(r2.area());
+console.log(Rectangle.impo());
