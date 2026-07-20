107bffe9cdd0ac73bed6f754ad85d500eb9bc886
Anirudh Tiwari <aniirudhtiwari@gmail.com>
2026-07-20 20:19:51 +0000

    promisified function

--- weeks/week4/promises.js
+++ weeks/week4/promises.js
@@ -17,13 +17,56 @@
 
 // setTimeout(callback,3000); //synchronous
 
-const fs = require("fs");
-function callback (err, data){
-    if(err){
-        console.log("error while reading the doc.");
-    }else{
-        console.log(data); 
-    }    
-}
-fs.readFile("a.txt", "utf-8", callback);
+// //synchronous readfile
+// const fs = require("fs");
+// function callback (err, data){
+//     if(err){
+//         console.log("error while reading the doc.");
+//     }else{
+//         console.log(data); 
+//     }    
+// }
+// fs.readFile("a.txt", "utf-8", callback);
 
+//asynchronous readfilepromisified
+
+const fs = require("fs");
+
+function fsReadFilePromisified(filePath, encoding) {      //promisifiedreadfile blackbox defined callback promise function
+    let p = new Promise((resolve, reject) => {
+        fs.readFile(filePath, encoding, (err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(data);
+            }
+        })
+    })
+    return p;
+}
+
+function callback(data){
+    console.log(data);
+}
+function callbackerr(err){
+    console.log("error while reading the file");   
+}
+let p = fsReadFilePromisified("a.txt","utf-8")
+.then(callback)
+.catch(callbackerr)
+
+/* calling a promisified function
+    
+    class promise{
+          constructor(fn){
+          
+          }
+          then(){
+          
+          }
+          catch(){ 
+          
+          }
+    }
+
+*/
--- weeks/week4/tempCodeRunnerFile.js
+++ weeks/week4/tempCodeRunnerFile.js
@@ -1,11 +1,1 @@
-// DATE CLASS 
-
-// const now= new Date();
-// console.log(now.toISOString);
-
-// //MAP CLASS
-
-// const map = new Map()
-// map.set('name','alice');
-// map.set('age', 30);
-// console.log(map.get('name'));
+.then(callback)
