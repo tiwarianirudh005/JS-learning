35f3614815436d2303093b0dc9eb7e517c58e57d
Anirudh Tiwari <aniirudhtiwari@gmail.com>
2026-07-23 18:22:07 +0000

    callback-hell-promisified

--- weeks/week4/promises.js
+++ weeks/week4/promises.js
@@ -1,74 +1,109 @@
-// PROMISE CLASS *IMP
-// PROMISIFIED SYNTAX FOR THE ASYNCHRONOUS PROGRAM
-// CALLBACK VERSION - SETTIMEOUT(CALLBACK, 3000);
-//PROMISIFIED VERSION - setTimeoutpromisified(3000).then(callback)
+// // PROMISE CLASS *IMP
+// // PROMISIFIED SYNTAX FOR THE ASYNCHRONOUS PROGRAM
+// // CALLBACK VERSION - SETTIMEOUT(CALLBACK, 3000);
+// //PROMISIFIED VERSION - setTimeoutpromisified(3000).then(callback)
 
-//using a function to return a promise
+// //using a function to return a promise
+
+// function setTimeoutPromisified(ms){
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+// function callback(){
+//     console.log("hi there");
+    
+// }
+// setTimeoutPromisified(3000).then(callback) //asynchronous promisified callback
+
+// setTimeout(callback,3000); //synchronous
+
+// // //synchronous readfile
+// const fs = require("fs");
+// function callback (err, data){
+//     if(err){
+//         console.log("error while reading the doc.");
+//     }else{
+//         console.log(data); 
+//     }    
+// }
+// fs.readFile("a.txt", "utf-8", callback);
+
+// //asynchronous readfilepromisified
+
+// const fs = require("fs");
+
+// function fsReadFilePromisified(filePath, encoding) {      //promisifiedreadfile blackbox defined callback promise function
+//     let p = new Promise((resolve, reject) => {
+//         fs.readFile(filePath, encoding, (err, data) => {
+//             if (err) {
+//                 reject(err)
+//             } else {
+//                 resolve(data);
+//             }
+//         })
+//     })
+//     return p;
+// }
+
+// function callback(data){
+//     console.log(data);
+// }
+// function callbackerr(err){
+//     console.log("error while reading the file");   
+// }
+// let p = fsReadFilePromisified("a.txt","utf-8")
+// .then(callback)
+// .catch(callbackerr)
+
+// /* calling a promisified function
+    
+//     class promise{
+//           constructor(fn){
+          
+//           }
+//           then(){
+          
+//           }
+//           catch(){ 
+          
+//           }
+//     }
+
+// */
+
+// //callback hell skipping with promisified version
+
+// // write a code that 1. log hi after 1 sec,log hello after 3 sec, log hello there 5 sec after step 2
+
+// //without callback not promisified
+// function step3 (){
+//     console.log("hello there");
+// }
+// function step2(){
+//     console.log("hello");
+//     setTimeout(step3,5000);
+// }
+// function step1(){
+//     console.log("hi");
+//     setTimeout(step2,3000);
+// }
+// setTimeout(step1, 1000);
+
+//with promisified
 
 function setTimeoutPromisified(ms){
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise (resolve => setTimeout(resolve,ms));
 }
 
-function callback(){
-    console.log("hi there");
-    
-}
-setTimeoutPromisified(3000).then(callback) //asynchronous promisified callback
-
-setTimeout(callback,3000); //synchronous
-
-// //synchronous readfile
-const fs = require("fs");
-function callback (err, data){
-    if(err){
-        console.log("error while reading the doc.");
-    }else{
-        console.log(data); 
-    }    
-}
-fs.readFile("a.txt", "utf-8", callback);
-
-//asynchronous readfilepromisified
-
-const fs = require("fs");
-
-function fsReadFilePromisified(filePath, encoding) {      //promisifiedreadfile blackbox defined callback promise function
-    let p = new Promise((resolve, reject) => {
-        fs.readFile(filePath, encoding, (err, data) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(data);
-            }
-        })
-    })
-    return p;
-}
-
-function callback(data){
-    console.log(data);
-}
-function callbackerr(err){
-    console.log("error while reading the file");   
-}
-let p = fsReadFilePromisified("a.txt","utf-8")
-.then(callback)
-.catch(callbackerr)
-
-/* calling a promisified function
-    
-    class promise{
-          constructor(fn){
-          
-          }
-          then(){
-          
-          }
-          catch(){ 
-          
-          }
-    }
-
-*/
-
-//callback hell, promises
+setTimeoutPromisified(1000)
+.then (function (){
+    console.log("hi");
+    return setTimeoutPromisified(3000);
+})
+.then (function () {
+    console.log("hello");
+    return setTimeoutPromisified(5000);
+})
+.then (function () {
+    console.log("hello there");
+});
