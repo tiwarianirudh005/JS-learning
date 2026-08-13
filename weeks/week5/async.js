2a13af3d6bba16d868f859fe1cac70ea5ca8cfaa
Anirudh <aniirudhtiwari@gmail.com>
2026-08-13 23:45:03 +0530

    practice

--- weeks/week5/async.js
+++ weeks/week5/async.js
@@ -1,15 +1,54 @@
-//assignment for async
+//assignment for async functions
 
 //q1. write a function that
 // 1.reads the content of file 
 // 2.trims the extra space from left and right
 // 3. write it back to the file 
 
-// code - synchronous approch
+// // code - synchronousfn call approch - it will hault other program which is not good
 const fs = require("fs")
-let contents = fs.readFileSync("content.txt", "utf-8");
-const trimmedcontents = contents.trim();
-fs.writeFileSync("content.txt", trimmedcontents);
+
+function cleanFilesync (filepath , cb){
+    let contents = fs.readFileSync("content.txt", "utf-8");
+    const trimmedcontents = contents.trim();
+    fs.writeFileSync("content.txt", trimmedcontents);
+    
+}
+
+cleanFilesync("content.txt", cb);
+
+// code - asynchronous fn callback approach- better way to write the program
+
+// const fs = require("fs")
+
+// function cleanFile(filePath, cb) {
+//     fs.readFile("content.txt","utf-8",function (err , content){
+//         const trimmedcontent = content.trim();
+//         fs.writeFile("content.txt", trimmedcontent, function(){
+//              cb();
+//         });
+//     });
+// }
+
+// cleanFile("content.txt", function(){
+//     console.log("done the cleaning!");
+    
+// });
+//promisified versiopn of this
+
+// const fs = require("fs")
+
+// function fsreadfilepromisified("content.txt","utf-8", function(filename, encoding){
+//     return new Promise(function (resolve, reject){
+//         fs.readFile(filename , encoding , function(err , content){
+//             if(err){
+//                 reject(err)
+//             }else {
+//                 resolve(content)
+//             }
+//         })
+//     })
+// })
 
 
 
--- weeks/week5/tempCodeRunnerFile.js
+++ weeks/week5/tempCodeRunnerFile.js
@@ -1,1 +1,13 @@
-const fs = require("fs");
+const fs = require("fs")
+
+// function fsreadfilepromisified("content.txt","utf-8", function(filename, encoding){
+//     return new Promise(function (resolve, reject){
+//         fs.readFile(filename , encoding , function(err , content){
+//             if(err){
+//                 reject(err)
+//             }else {
+//                 resolve(content)
+//             }
+//         })
+//     })
+// })
